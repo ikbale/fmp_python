80ca1dbeef4f31ba20a5e8e06a3ea4894c6aff42
ikbale <ikbale94@gmail.com>
2020-06-17 18:15:44 +0100

    add support format_data

--- fmp_python/fmp.py
+++ fmp_python/fmp.py
@@ -1,25 +1,45 @@
 import pandas as pd
 import requests
 import os
+import io
 
 class FMP(object):
-    _FMP_API_BASE_URL = "https://financialmodelingprep.com/api/v3/"
+    """
+    Base class that implements  api calls 
+    """
+    _API_VERSION = "v3/"
+    _FMP_API_BASE_URL = "https://financialmodelingprep.com/api/"+_API_VERSION
+    _INDEX_SYMBOL = "^"
 
+    class FMPException(Exception):
+        pass
 
     def __init__(self, api_key=None):
         self.api_key = api_key or os.getenv('FMP_API_KEY')
 
 
-    def get_quote_short(self, symbol, output_format='json'):
+    def get_quote_short(self, symbol):
         quote = requests.get(FMP._FMP_API_BASE_URL+"/quote-short/"+symbol+"?apikey="+self.api_key)
-        if output_format == 'json':
-            quote = quote.json()
         return quote
+
+    def get_index_quote(self,symbol):
+        index_quote = requests.get(FMP._FMP_API_BASE_URL+"/quote/"+FMP._INDEX_SYMBOL+symbol+"?apikey="+self.api_key)
+        return index_quote
+
+    def format_data(self, data, output_format='pandas'):
+        if output_format=='json':
+            data = data.json()
+        elif output_format=='pandas':
+            data = pd.read_csv(io.StringIO(data.content.decode('UTF-8')))
+        else:
+            raise FMP.FMPException("FMP.format_data: output must be one of pandas or json")
+
 
 
 def main():
     fmp = FMP()
-    print(fmp.get_quote_short('AAL'))
+    indexes = fmp.get_quote_short('AAL')
+    fmp.format_data(indexes)
 
 if __name__ == "__main__":
     main()
