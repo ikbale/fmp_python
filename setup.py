a8f1b1c6b8b0ce6c091e36320a252ecc86c8afee
Giovanni <giovanni@gslab.it>
2022-08-27 16:49:43 +0200

    * add date from_, to params to get_historical_price * fix possible history key missing in json. * code formatting

--- setup.py
+++ setup.py
@@ -4,27 +4,27 @@ from os import path
 
 current_dir = path.abspath(path.dirname(__file__))
 
-# Get the README content 
+# Get the README content
 with io.open(path.join(current_dir, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name = 'fmp_python',
-    version = '0.1.4',
-    description = 'Python wrapper for Financial Modeling Prep API',
+    name='fmp_python',
+    version='0.1.5',
+    description='Python wrapper for Financial Modeling Prep API',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    keywords = ' python finance trading stocks prices financialmodelingprep api fmp alphavantage wtd iex',
-    url = 'https://github.com/ikbale/fmp_python',
-    author = 'Ikbale Maghraoui',
-    author_email = 'ikbale94@gmail.com',
-    license = 'MIT',
-    packages = find_packages(),
-    install_requires = [
+    keywords=' python finance trading stocks prices financialmodelingprep api fmp alphavantage wtd iex',
+    url='https://github.com/ikbale/fmp_python',
+    author='Ikbale Maghraoui',
+    author_email='ikbale94@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
         'pandas',
-		'requests',
+        'requests',
     ],
-    test_requires = [
+    test_requires=[
         'requests_mock'
     ],
     classifiers=[
@@ -36,6 +36,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    include_package_data = False,
-    zip_safe = False
+    include_package_data=False,
+    zip_safe=False
 )
