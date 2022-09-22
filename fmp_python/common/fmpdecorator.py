6b47e3bcf7e3e7327005480f5006b43bc1d75f9c
Giovanni <giovanni@gslab.it>
2022-09-22 15:41:56 +0200

    * fix outdir for linux platform * add date param for get_historical_chart

--- fmp_python/common/fmpdecorator.py
+++ fmp_python/common/fmpdecorator.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import functools
 import pandas as pd
 from datetime import datetime
@@ -73,9 +75,8 @@ class FMPDecorator:
         current_full_date = datetime.now().strftime("%d-%m-%Y_%Hh%Mmin%Ss")
 
         filename = '_'.join([symbol, current_full_date])
-        outdir = os.path.join('C:', 'tmp', category, current_day)
+        _root = '{}'.format('C:' if sys.platform == 'win32' else '/')
+        outdir = os.path.join(_root, 'tmp', category, current_day)
         os.makedirs(outdir, exist_ok=True)
-
         fullname = os.path.join(outdir, filename + '.xlsx')
-
         return fullname
